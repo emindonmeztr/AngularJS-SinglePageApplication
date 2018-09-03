e0380f21ca5d4ad3be6c5d43bc4a4a8597375b84
emindonmeztr <emindonmezz@outlook.com>
2018-09-03 18:22:09 +0300

    Third Commit
    Transclude & Replace
    Animations
    Form validations
    Location services
    Pretty URL's

--- app/app.js
+++ app/app.js
@@ -1,9 +1,17 @@
-var myBookStoreApp = angular.module('myBookStoreApp', ['ngRoute']);
-myBookStoreApp.config(['$routeProvider', function($routeProvider){
-
+var myBookStoreApp = angular.module('myBookStoreApp', ['ngRoute', 'ngAnimate']);
+myBookStoreApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+$locationProvider.html5Mode(true);
 $routeProvider
 .when('/home', {
   templateUrl: 'views/home.html'
+})
+.when('/contact', {
+  templateUrl: 'views/contact.html',
+  controller: 'ContactController'
+})
+.when('/contact-success', {
+  templateUrl: 'views/contact-success.html',
+  controller: 'ContactController'
 })
 .when('/directory', {
   templateUrl: 'views/directory.html',
@@ -38,32 +46,36 @@ $scope.newbook.author ="";
 $scope.newbook.thumb ="";
 };
 
+$scope.removeAll = function(){
+ $scope.books = [];
+};
+
 $scope.books = [
   {
   	name: "Anna Karenina",
   	belt: "brown",
-  	price: 1,
+  	price: 76.82,
     author: "Leo Tolstoy",
   	available: true,
   	thumb: "content/img/image.png"
   }, {
   	name: "Madame Bovary",
   	belt: "red",
-  	price: 2,
+  	price: 39.37,
     author: "Gustave Flaubert",
   	available: true,
   	thumb: "content/img/image.png"
   }, {
   	name: "War and Peace",
   	belt: "orange",
-  	price: 3,
+  	price: 89.93,
     author: "Leo Tolstoy",
   	available: true,
   	thumb: "content/img/image.png"
   }, {
   	name: "The Great Gatsby",
   	belt: "black",
-  	price: 4,
+  	price: 67.49,
     author: "F.Scott Fitzgerald",
   	available: true,
   	thumb: "content/img/image.png"
@@ -71,7 +83,7 @@ $scope.books = [
   , {
   	name: "Lolita",
   	belt: "yellow",
-  	price: 5,
+  	price: 66.10,
     author: "Vladimir Nabokov",
   	available: true,
   	thumb: "content/img/image.png"
@@ -79,7 +91,7 @@ $scope.books = [
   , {
   	name: "Middlemarch",
   	belt: "pink",
-  	price: 6,
+  	price: 68.22,
     author: "George Eliot",
   	available: true,
   	thumb: "content/img/image.png"
@@ -87,7 +99,7 @@ $scope.books = [
   , {
   	name: "The Adventures of Huckleberry Finn",
   	belt: "purple",
-  	price: 7,
+  	price: 19.79,
     author: "Mark Twain",
   	available: true,
   	thumb: "content/img/image.png"
@@ -95,7 +107,7 @@ $scope.books = [
   , {
     name: "The Stories of Anton Chekhov",
     belt: "magenta",
-    price: 8,
+    price: 95.29,
     author: "Anton Chekhov",
     available: true,
     thumb: "content/img/image.png"
@@ -103,7 +115,7 @@ $scope.books = [
   , {
     name: "In Search of Lost Time",
     belt: "silver",
-    price: 9,
+    price: 151.53,
     author: "Marcel Proust",
     available: true,
     thumb: "content/img/image.png"
@@ -111,11 +123,19 @@ $scope.books = [
   , {
     name: "Hamlet",
     belt: "gold",
-    price: 10,
+    price: 37.76,
     author: "William Shakespeare",
     available: true,
     thumb: "content/img/image.png"
   }
 ];
+
+}]);
+
+myBookStoreApp.controller('ContactController', ['$scope', '$location', function($scope, $location){
+
+$scope.sendMessage = function(){
+  $location.path('/contact-success');
+};
 
 }]);
